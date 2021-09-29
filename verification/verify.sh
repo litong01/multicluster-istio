796805bebfc8e4f2c3bbbe4d5c326e93e2c63792
Tong Li <litong01@us.ibm.com>
2021-09-28 21:26:44 -0400

    Fixed the following issues for the verification process
    1. Do not create the namespace over and over
    2. Wait for the pods to be ready before moving on
    3. Removed the wait loop using kubectl wait instead
    
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- verification/verify.sh
+++ verification/verify.sh
@@ -10,8 +10,10 @@ CTX_CLUSTER1=kind-cluster1
 CTX_CLUSTER2=kind-cluster2
 
 # Create namespaces in each cluster
-kubectl create --context="${CTX_CLUSTER1}" namespace sample
-kubectl create --context="${CTX_CLUSTER2}" namespace sample
+kubectl create --context="${CTX_CLUSTER1}" namespace sample --dry-run=client -o yaml \
+    | kubectl apply --context="${CTX_CLUSTER1}" -f -
+kubectl create --context="${CTX_CLUSTER2}" namespace sample --dry-run=client -o yaml \
+    | kubectl apply --context="${CTX_CLUSTER2}" -f -
 
 # Label the namespace for istio injection
 kubectl label --context="${CTX_CLUSTER1}" namespace sample \
@@ -21,40 +23,31 @@ kubectl label --context="${CTX_CLUSTER2}" namespace sample \
 
 # Create hello world services in each cluster
 kubectl apply --context="${CTX_CLUSTER1}" \
-    -f helloworld.yaml \
-    -l service=helloworld -n sample
+    -f helloworld.yaml -l service=helloworld -n sample
 kubectl apply --context="${CTX_CLUSTER2}" \
-    -f helloworld.yaml \
-    -l service=helloworld -n sample
+    -f helloworld.yaml -l service=helloworld -n sample
 
 # Deploy hello world V1 in the first cluster
 kubectl apply --context="${CTX_CLUSTER1}" \
-    -f helloworld.yaml \
-    -l version=v1 -n sample
+    -f helloworld.yaml -l version=v1 -n sample
 
 # Deploy hello world V1 in the second cluster
 kubectl apply --context="${CTX_CLUSTER2}" \
-    -f helloworld.yaml \
-    -l version=v2 -n sample
+    -f helloworld.yaml -l version=v2 -n sample
 
 # Deploy the sleep app for checking the hello world app
-kubectl apply --context="${CTX_CLUSTER1}" \
-    -f sleep.yaml -n sample
-kubectl apply --context="${CTX_CLUSTER2}" \
-    -f sleep.yaml -n sample
+kubectl apply --context="${CTX_CLUSTER1}" -f sleep.yaml -n sample
+kubectl apply --context="${CTX_CLUSTER2}" -f sleep.yaml -n sample
+
+# Wait for the pod to be ready in the first cluster
+kubectl wait --context="${CTX_CLUSTER1}" -n sample pod -l app=sleep --for=condition=Ready --timeout=60s
 
 # Get the pod name in the first cluster
-while : ; do
-  PODNAME=$(kubectl get pod --context="${CTX_CLUSTER1}" -n sample -l \
-            app=sleep -o jsonpath='{.items[0].metadata.name}')
-  if [[ ! -z ${PODNAME} ]]; then
-    break
-  fi
-  echo -e ${Green}Waiting${ColorOff} for Sleep pod to be ready...
-  sleep 3
-done
+PODNAME=$(kubectl get pod --context="${CTX_CLUSTER1}" -n sample -l \
+          app=sleep -o jsonpath='{.items[0].metadata.name}')
 
 # Send 5 requests from the Sleep pod on cluster1 to the HelloWorld service
+echo -e ${Green}Ready to hit the service from the first cluster ${ColorOff}
 x=1; while [ $x -le 5 ]; do
   kubectl exec --context="${CTX_CLUSTER1}" -n sample -c sleep ${PODNAME} \
     -- curl -sS helloworld.sample:5000/hello
@@ -62,18 +55,15 @@ x=1; while [ $x -le 5 ]; do
   x=$(( $x + 1 ))
 done
 
+# Wait for the pod to be ready in the second cluster
+kubectl wait --context="${CTX_CLUSTER2}" -n sample pod -l app=sleep --for=condition=Ready --timeout=60s
+
 # Get the pod name in the second cluster
-while : ; do
-  PODNAME=$(kubectl get pod --context="${CTX_CLUSTER2}" -n sample -l \
-            app=sleep -o jsonpath='{.items[0].metadata.name}')
-  if [[ ! -z ${PODNAME} ]]; then
-    break
-  fi
-  echo -e ${Green}Waiting${ColorOff} for Sleep pod to be ready...
-  sleep 3
-done
+PODNAME=$(kubectl get pod --context="${CTX_CLUSTER2}" -n sample -l \
+          app=sleep -o jsonpath='{.items[0].metadata.name}')
 
 # Send 5 requests from the Sleep pod on cluster1 to the HelloWorld service
+echo -e ${Green}Ready to hit the service from the second cluster ${ColorOff}
 x=1; while [ $x -le 5 ]; do
   kubectl exec --context="${CTX_CLUSTER2}" -n sample -c sleep ${PODNAME} \
     -- curl -sS helloworld.sample:5000/hello
