acfcbbebb13dc1f17c4c7d2b8a9d505d6eb496b6
Tong Li <litong01@us.ibm.com>
2022-01-10 14:57:28 -0500

    Expose various endpoints
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- kiali/do-external-control-plane.sh
+++ kiali/do-external-control-plane.sh
@@ -13,10 +13,10 @@ if [[ $1 != '' ]]; then
   ACTION=delete
 fi
 
-
-# install prometheus and configure it to scrape from remote cluster
-# the prometheus will be installed onto cluster1 and external-istiod namespace
-kubectl ${ACTION} --context="kind-${CLUSTER1_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
+# Install prometheus and configure it to scrape in the remote cluster
+# this prometheus will be installed onto cluster2, external-istiod namespace
+echo "Setting up prometheus services in cluster ${CLUSTER2_NAME}..."
+kubectl ${ACTION} --context="kind-${CLUSTER2_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
 ---
 # Source: prometheus/templates/server/serviceaccount.yaml
 apiVersion: v1
@@ -270,10 +270,9 @@ data:
         regex: Pending|Succeeded|Failed|Completed
         source_labels:
         - __meta_kubernetes_pod_phase
-    - job_name: kubernetes-pods-slow-remote
+    - job_name: kubernetes-pods-slow
       kubernetes_sd_configs:
       - role: pod
-        kubeconfig_file: /var/run/secrets/remote/config
       relabel_configs:
       - action: keep
         regex: true
@@ -312,6 +311,520 @@ data:
         - __meta_kubernetes_pod_phase
       scrape_interval: 5m
       scrape_timeout: 30s
+  recording_rules.yml: |
+    {}
+  rules: |
+    {}
+---
+# Source: prometheus/templates/server/clusterrole.yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes
+      - nodes/proxy
+      - nodes/metrics
+      - services
+      - endpoints
+      - pods
+      - ingresses
+      - configmaps
+    verbs:
+      - get
+      - list
+      - watch
+  - apiGroups:
+      - "extensions"
+      - "networking.k8s.io"
+    resources:
+      - ingresses/status
+      - ingresses
+    verbs:
+      - get
+      - list
+      - watch
+  - nonResourceURLs:
+      - "/metrics"
+    verbs:
+      - get
+---
+# Source: prometheus/templates/server/clusterrolebinding.yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+subjects:
+  - kind: ServiceAccount
+    name: prometheus
+    namespace: ${ISTIO_NAMESPACE}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: prometheus
+---
+# Source: prometheus/templates/server/service.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+spec:
+  ports:
+    - name: http
+      port: 9090
+      protocol: TCP
+      targetPort: 9090
+  selector:
+    component: "server"
+    app: prometheus
+    release: prometheus
+  sessionAffinity: None
+  type: "ClusterIP"
+---
+# Source: prometheus/templates/server/deploy.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+spec:
+  selector:
+    matchLabels:
+      component: "server"
+      app: prometheus
+      release: prometheus
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        component: "server"
+        app: prometheus
+        release: prometheus
+        chart: prometheus-14.6.1
+        heritage: Helm
+
+        sidecar.istio.io/inject: "false"
+    spec:
+      enableServiceLinks: true
+      serviceAccountName: prometheus
+      containers:
+        - name: prometheus-server-configmap-reload
+          image: "jimmidyson/configmap-reload:v0.5.0"
+          imagePullPolicy: "IfNotPresent"
+          args:
+            - --volume-dir=/etc/config
+            - --webhook-url=http://127.0.0.1:9090/-/reload
+          resources:
+            {}
+          volumeMounts:
+            - name: config-volume
+              mountPath: /etc/config
+              readOnly: true
+
+        - name: prometheus-server
+          image: "prom/prometheus:v2.26.0"
+          imagePullPolicy: "IfNotPresent"
+          args:
+            - --storage.tsdb.retention.time=15d
+            - --config.file=/etc/config/prometheus.yml
+            - --storage.tsdb.path=/data
+            - --web.console.libraries=/etc/prometheus/console_libraries
+            - --web.console.templates=/etc/prometheus/consoles
+            - --web.enable-lifecycle
+          ports:
+            - containerPort: 9090
+          readinessProbe:
+            httpGet:
+              path: /-/ready
+              port: 9090
+            initialDelaySeconds: 0
+            periodSeconds: 5
+            timeoutSeconds: 4
+            failureThreshold: 3
+            successThreshold: 1
+          livenessProbe:
+            httpGet:
+              path: /-/healthy
+              port: 9090
+            initialDelaySeconds: 30
+            periodSeconds: 15
+            timeoutSeconds: 10
+            failureThreshold: 3
+            successThreshold: 1
+          resources:
+            {}
+          volumeMounts:
+            - name: config-volume
+              mountPath: /etc/config
+            - name: storage-volume
+              mountPath: /data
+              subPath: ""
+      hostNetwork: false
+      dnsPolicy: ClusterFirst
+      securityContext:
+        fsGroup: 65534
+        runAsGroup: 65534
+        runAsNonRoot: true
+        runAsUser: 65534
+      terminationGracePeriodSeconds: 300
+      volumes:
+        - name: config-volume
+          configMap:
+            name: prometheus
+        - name: storage-volume
+          emptyDir:
+            {}
+EOF
+
+# Create the load balancer to expose prometheus service
+echo "Creating load balancer for prometheus in cluster ${CLUSTER2_NAME}"
+kubectl ${ACTION} --context="kind-${CLUSTER2_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: prometheus-endpoint-service
+spec:
+  type: LoadBalancer
+  selector:
+    app: prometheus
+    release: prometheus
+  ports:
+  - name: uiport
+    port: 20002
+    targetPort: 9090
+EOF
+
+# Wait for the public IP address to be allocated
+if [[ $ACTION != 'delete' ]]; then
+while : ; do
+  PROMETHEUS_ADDR=$(kubectl get --context kind-${CLUSTER2_NAME} -n $ISTIO_NAMESPACE \
+    services/prometheus-endpoint-service -o jsonpath='{ .status.loadBalancer.ingress[0].ip}')
+
+  if [[ ! -z $PROMETHEUS_ADDR ]]; then
+      echo "Public IP address ${PROMETHEUS_ADDR} is now available"
+      break
+  fi
+  echo 'Waiting for public IP address to be available...'
+  sleep 3
+done
+fi
+
+# install prometheus and configure it to scrape in external cluster (control plane)
+# the prometheus will be installed onto cluster1 and external-istiod namespace
+echo "Setting up prometheus in cluster ${CLUSTER1_NAME} for Kaili"
+kubectl ${ACTION} --context="kind-${CLUSTER1_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
+---
+# Source: prometheus/templates/server/serviceaccount.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+  annotations:
+    {}
+---
+# Source: prometheus/templates/server/cm.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  labels:
+    component: "server"
+    app: prometheus
+    release: prometheus
+    chart: prometheus-14.6.1
+    heritage: Helm
+  name: prometheus
+data:
+  alerting_rules.yml: |
+    {}
+  alerts: |
+    {}
+  prometheus.yml: |
+    global:
+      evaluation_interval: 1m
+      scrape_interval: 15s
+      scrape_timeout: 10s
+    rule_files:
+    - /etc/config/recording_rules.yml
+    - /etc/config/alerting_rules.yml
+    - /etc/config/rules
+    - /etc/config/alerts
+    scrape_configs:
+    - job_name: prometheus
+      static_configs:
+      - targets:
+        - localhost:9090
+    - job_name: prometheus_remote
+      static_configs:
+      - targets:
+        - ${PROMETHEUS_ADDR}:20002
+    - bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+      job_name: kubernetes-apiservers
+      kubernetes_sd_configs:
+      - role: endpoints
+      relabel_configs:
+      - action: keep
+        regex: default;kubernetes;https
+        source_labels:
+        - __meta_kubernetes_namespace
+        - __meta_kubernetes_service_name
+        - __meta_kubernetes_endpoint_port_name
+      scheme: https
+      tls_config:
+        ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+        insecure_skip_verify: true
+    - bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+      job_name: kubernetes-nodes
+      kubernetes_sd_configs:
+      - role: node
+      relabel_configs:
+      - action: labelmap
+        regex: __meta_kubernetes_node_label_(.+)
+      - replacement: kubernetes.default.svc:443
+        target_label: __address__
+      - regex: (.+)
+        replacement: /api/v1/nodes/$1/proxy/metrics
+        source_labels:
+        - __meta_kubernetes_node_name
+        target_label: __metrics_path__
+      scheme: https
+      tls_config:
+        ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+        insecure_skip_verify: true
+    - bearer_token_file: /var/run/secrets/kubernetes.io/serviceaccount/token
+      job_name: kubernetes-nodes-cadvisor
+      kubernetes_sd_configs:
+      - role: node
+      relabel_configs:
+      - action: labelmap
+        regex: __meta_kubernetes_node_label_(.+)
+      - replacement: kubernetes.default.svc:443
+        target_label: __address__
+      - regex: (.+)
+        replacement: /api/v1/nodes/$1/proxy/metrics/cadvisor
+        source_labels:
+        - __meta_kubernetes_node_name
+        target_label: __metrics_path__
+      scheme: https
+      tls_config:
+        ca_file: /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+        insecure_skip_verify: true
+    - job_name: kubernetes-service-endpoints
+      kubernetes_sd_configs:
+      - role: endpoints
+      relabel_configs:
+      - action: keep
+        regex: true
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_scrape
+      - action: replace
+        regex: (https?)
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_scheme
+        target_label: __scheme__
+      - action: replace
+        regex: (.+)
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_path
+        target_label: __metrics_path__
+      - action: replace
+        regex: ([^:]+)(?::\d+)?;(\d+)
+        replacement: "$1:$2"
+        source_labels:
+        - __address__
+        - __meta_kubernetes_service_annotation_prometheus_io_port
+        target_label: __address__
+      - action: labelmap
+        regex: __meta_kubernetes_service_label_(.+)
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_namespace
+        target_label: kubernetes_namespace
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_service_name
+        target_label: kubernetes_name
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_pod_node_name
+        target_label: kubernetes_node
+    - job_name: kubernetes-service-endpoints-slow
+      kubernetes_sd_configs:
+      - role: endpoints
+      relabel_configs:
+      - action: keep
+        regex: true
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_scrape_slow
+      - action: replace
+        regex: (https?)
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_scheme
+        target_label: __scheme__
+      - action: replace
+        regex: (.+)
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_path
+        target_label: __metrics_path__
+      - action: replace
+        regex: ([^:]+)(?::\d+)?;(\d+)
+        replacement: "$1:$2"
+        source_labels:
+        - __address__
+        - __meta_kubernetes_service_annotation_prometheus_io_port
+        target_label: __address__
+      - action: labelmap
+        regex: __meta_kubernetes_service_label_(.+)
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_namespace
+        target_label: kubernetes_namespace
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_service_name
+        target_label: kubernetes_name
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_pod_node_name
+        target_label: kubernetes_node
+      scrape_interval: 5m
+      scrape_timeout: 30s
+    - honor_labels: true
+      job_name: prometheus-pushgateway
+      kubernetes_sd_configs:
+      - role: service
+      relabel_configs:
+      - action: keep
+        regex: pushgateway
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_probe
+    - job_name: kubernetes-services
+      kubernetes_sd_configs:
+      - role: service
+      metrics_path: /probe
+      params:
+        module:
+        - http_2xx
+      relabel_configs:
+      - action: keep
+        regex: true
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_probe
+      - source_labels:
+        - __address__
+        target_label: __param_target
+      - replacement: blackbox
+        target_label: __address__
+      - source_labels:
+        - __param_target
+        target_label: instance
+      - action: labelmap
+        regex: __meta_kubernetes_service_label_(.+)
+      - source_labels:
+        - __meta_kubernetes_namespace
+        target_label: kubernetes_namespace
+      - source_labels:
+        - __meta_kubernetes_service_name
+        target_label: kubernetes_name
+    - job_name: kubernetes-pods
+      kubernetes_sd_configs:
+      - role: pod
+      relabel_configs:
+      - action: keep
+        regex: true
+        source_labels:
+        - __meta_kubernetes_pod_annotation_prometheus_io_scrape
+      - action: replace
+        regex: (https?)
+        source_labels:
+        - __meta_kubernetes_pod_annotation_prometheus_io_scheme
+        target_label: __scheme__
+      - action: replace
+        regex: (.+)
+        source_labels:
+        - __meta_kubernetes_pod_annotation_prometheus_io_path
+        target_label: __metrics_path__
+      - action: replace
+        regex: ([^:]+)(?::\d+)?;(\d+)
+        replacement: "$1:$2"
+        source_labels:
+        - __address__
+        - __meta_kubernetes_pod_annotation_prometheus_io_port
+        target_label: __address__
+      - action: labelmap
+        regex: __meta_kubernetes_pod_label_(.+)
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_namespace
+        target_label: kubernetes_namespace
+      - action: replace
+        source_labels:
+        - __meta_kubernetes_pod_name
+        target_label: kubernetes_pod_name
+      - action: drop
+        regex: Pending|Succeeded|Failed|Completed
+        source_labels:
+        - __meta_kubernetes_pod_phase
+    - job_name: kubernetes-services-remote
+      kubernetes_sd_configs:
+      - role: service
+        kubeconfig_file: /var/run/secrets/remote/config
+      metrics_path: /probe
+      params:
+        module:
+          - http_2xx
+      relabel_configs:
+      - action: keep
+        regex: true
+        source_labels:
+        - __meta_kubernetes_service_annotation_prometheus_io_probe
+      - source_labels:
+        - __address__
+        target_label: __param_target
+      - replacement: blackbox
+        target_label: __address__
+      - source_labels:
+        - __param_target
+        target_label: instance
+      - action: labelmap
+        regex: __meta_kubernetes_service_label_(.+)
+      - source_labels:
+        - __meta_kubernetes_namespace
+        target_label: kubernetes_namespace
+      - source_labels:
+        - __meta_kubernetes_service_name
+        target_label: kubernetes_name
   recording_rules.yml: |
     {}
   rules: |
@@ -510,7 +1023,26 @@ spec:
             secretName: istio-kubeconfig
 EOF
 
+# Create the load balancer to expose prometheus service
+echo "Creating load balancer for prometheus in cluster ${CLUSTER1_NAME}"
+kubectl ${ACTION} --context="kind-${CLUSTER1_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: prometheus-endpoint-service
+spec:
+  type: LoadBalancer
+  selector:
+    app: prometheus
+    release: prometheus
+  ports:
+  - name: uiport
+    port: 20003
+    targetPort: 9090
+EOF
 
+# Now setup kaili
+echo "Setting up kaili in ${CLUSTER1_NAME}"
 kubectl ${ACTION} --context="kind-${CLUSTER1_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
 ---
 # Source: kiali-server/templates/serviceaccount.yaml
@@ -1048,6 +1580,8 @@ spec:
 ...
 EOF
 
+# Create load balancer for kiali dashboard
+echo "Create load balancer for Kiali dashboard..."
 kubectl ${ACTION} --context="kind-${CLUSTER1_NAME}" -n "${ISTIO_NAMESPACE}" -f - <<EOF
 apiVersion: v1
 kind: Service
--- nginx/proxytraffic.sh
+++ nginx/proxytraffic.sh
@@ -15,6 +15,14 @@
 CLUSTER1_NAME=cluster1
 CLUSTER2_NAME=cluster2
 ISTIO_NAMESPACE=external-istiod
+if [[ $1 != '' ]]; then
+    docker rm -f myprometheus1 myprometheus2 mykiali
+    exit 0
+fi
+
+
+
+
 
 LB_IP=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
   kiali-endpoint-service \
@@ -23,9 +31,7 @@ LB_PORT=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services
   kiali-endpoint-service \
   -o jsonpath='{ .spec.ports[0].port }')
 
-echo "Traffic will be proxyed to $LB_IP:$LB_PORT"
-
-cat <<EOF > nginx.conf
+cat <<EOF > nginx_kiali.conf
 worker_processes  5;
 
 events {
@@ -54,5 +60,88 @@ EOF
 
 # Run the nginx proxy on the same docker network where k8s is
 # also running on, by default it should be called kind
-docker run --name myproxy -d -p 8080:80 --network kind \
-  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro nginx:latest
+docker run --name mykiali -d -p 8080:80 --network kind \
+  -v $(pwd)/nginx_kiali.conf:/etc/nginx/nginx.conf:ro nginx:latest
+
+
+# Get the load balancer IP and port for the prometheus IP and Port in cluster2
+LB_IP=$(kubectl get --context kind-$CLUSTER2_NAME -n $ISTIO_NAMESPACE services \
+  prometheus-endpoint-service \
+  -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
+LB_PORT=$(kubectl get --context kind-$CLUSTER2_NAME -n $ISTIO_NAMESPACE services \
+  prometheus-endpoint-service \
+  -o jsonpath='{ .spec.ports[0].port }')
+
+# Create the prometheus nginx configuration file
+cat <<EOF > nginx_prometheus2.conf
+worker_processes  5;
+
+events {
+  worker_connections  4096;
+}
+
+http {
+  index    index.html index.htm index.php;
+
+  default_type application/octet-stream;
+  sendfile     on;
+  tcp_nopush   on;
+  server_names_hash_bucket_size 128;
+
+  server {
+    listen       80;
+    server_name  domain1.com www.domain1.com;
+    root         html;
+
+    location / {
+      proxy_pass   http://${LB_IP}:${LB_PORT};
+    }
+  }
+}
+EOF
+
+# Run the nginx proxy on the same docker network where k8s is
+# also running on, by default it should be called kind
+docker run --name myprometheus2 -d -p 9090:80 --network kind \
+  -v $(pwd)/nginx_prometheus2.conf:/etc/nginx/nginx.conf:ro nginx:latest
+
+
+# Get the load balancer IP and port for the prometheus IP and Port in cluster1
+LB_IP=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
+  prometheus-endpoint-service \
+  -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
+LB_PORT=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
+  prometheus-endpoint-service \
+  -o jsonpath='{ .spec.ports[0].port }')
+
+cat <<EOF > nginx_prometheus1.conf
+worker_processes  5;
+
+events {
+  worker_connections  4096;
+}
+
+http {
+  index    index.html index.htm index.php;
+
+  default_type application/octet-stream;
+  sendfile     on;
+  tcp_nopush   on;
+  server_names_hash_bucket_size 128;
+
+  server {
+    listen       80;
+    server_name  domain1.com www.domain1.com;
+    root         html;
+
+    location / {
+      proxy_pass   http://${LB_IP}:${LB_PORT};
+    }
+  }
+}
+EOF
+
+# Run the nginx proxy on the same docker network where k8s is
+# also running on, by default it should be called kind
+docker run --name myprometheus1 -d -p 9080:80 --network kind \
+  -v $(pwd)/nginx_prometheus1.conf:/etc/nginx/nginx.conf:ro nginx:latest
