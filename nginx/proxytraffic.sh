b66ebd5bb1d72385bdfabb27f8817d154b02b829
Tong Li <litong01@us.ibm.com>
2022-01-20 13:39:48 -0500

    refactored the nginx proxy script
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- nginx/proxytraffic.sh
+++ nginx/proxytraffic.sh
@@ -12,26 +12,27 @@
 # you will need to setup LB_IP and LB_PORT based on your
 # own services
 
-CLUSTER1_NAME=cluster1
-CLUSTER2_NAME=cluster2
 ISTIO_NAMESPACE=external-istiod
 if [[ $1 != '' ]]; then
-    docker rm -f myprometheus1 myprometheus2 mykiali
+    docker rm -f istioproxy
     exit 0
 fi
 
+declare -A ALL
+ALL=(
+    [clusters]='kind-cluster1,kind-cluster1,kind-cluster2'
+    [services]='kiali-endpoint-service,prometheus-endpoint-service,prometheus-endpoint-service'
+)
+cmds='clusters services'
 
+for i in ${cmds[@]}; do
+   key=$(echo "${i,,}"|xargs)
+   val=$(echo "${ALL[$key]}")
+   IFS=',' read -r -a "${key^}" <<< "${val}"
+done
 
-
-
-LB_IP=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
-  kiali-endpoint-service \
-  -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
-LB_PORT=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
-  kiali-endpoint-service \
-  -o jsonpath='{ .spec.ports[0].port }')
-
-cat <<EOF > nginx_kiali.conf
+# Create the first part of the nginx configuration
+cat <<EOF > /tmp/istioproxy.conf
 worker_processes  5;
 
 events {
@@ -45,103 +46,43 @@ http {
   sendfile     on;
   tcp_nopush   on;
   server_names_hash_bucket_size 128;
+EOF
 
+
+# Now generate the nginx proxy configuration file
+STARTPORT=8080
+PORTMAP=''
+for aservice in "${Services[@]}"; do
+  index=$(($STARTPORT-8080))
+  LB_IP=$(kubectl get --context ${Clusters[$index]} -n $ISTIO_NAMESPACE services \
+    ${aservice} -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
+  LB_PORT=$(kubectl get --context ${Clusters[$index]} -n $ISTIO_NAMESPACE services \
+    ${aservice} -o jsonpath='{ .spec.ports[0].port }')
+
+  cat <<EOF >> /tmp/istioproxy.conf
+  
   server {
-    listen       80;
-    server_name  domain1.com www.domain1.com;
+    listen       ${STARTPORT};
+    server_name  domain${STARTPORT}.com;
     root         html;
-
     location / {
       proxy_pass   http://${LB_IP}:${LB_PORT};
     }
   }
+EOF
+   # Display service mapped port
+   echo "${aservice^}@${Clusters[$index]} mapped to port: ${STARTPORT}"
+   # Generate the port map parameter
+   PORTMAP="${PORTMAP} -p ${STARTPORT}:${STARTPORT}"
+
+   # Increase the port number 
+   STARTPORT=$((STARTPORT+1))
+done
+
+cat <<EOF >> /tmp/istioproxy.conf
 }
 EOF
 
-# Run the nginx proxy on the same docker network where k8s is
-# also running on, by default it should be called kind
-docker run --name mykiali -d -p 8080:80 --network kind \
-  -v $(pwd)/nginx_kiali.conf:/etc/nginx/nginx.conf:ro nginx:latest
-
-
-# Get the load balancer IP and port for the prometheus IP and Port in cluster2
-LB_IP=$(kubectl get --context kind-$CLUSTER2_NAME -n $ISTIO_NAMESPACE services \
-  prometheus-endpoint-service \
-  -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
-LB_PORT=$(kubectl get --context kind-$CLUSTER2_NAME -n $ISTIO_NAMESPACE services \
-  prometheus-endpoint-service \
-  -o jsonpath='{ .spec.ports[0].port }')
-
-# Create the prometheus nginx configuration file
-cat <<EOF > nginx_prometheus2.conf
-worker_processes  5;
-
-events {
-  worker_connections  4096;
-}
-
-http {
-  index    index.html index.htm index.php;
-
-  default_type application/octet-stream;
-  sendfile     on;
-  tcp_nopush   on;
-  server_names_hash_bucket_size 128;
-
-  server {
-    listen       80;
-    server_name  domain1.com www.domain1.com;
-    root         html;
-
-    location / {
-      proxy_pass   http://${LB_IP}:${LB_PORT};
-    }
-  }
-}
-EOF
-
-# Run the nginx proxy on the same docker network where k8s is
-# also running on, by default it should be called kind
-docker run --name myprometheus2 -d -p 9090:80 --network kind \
-  -v $(pwd)/nginx_prometheus2.conf:/etc/nginx/nginx.conf:ro nginx:latest
-
-
-# Get the load balancer IP and port for the prometheus IP and Port in cluster1
-LB_IP=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
-  prometheus-endpoint-service \
-  -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
-LB_PORT=$(kubectl get --context kind-$CLUSTER1_NAME -n $ISTIO_NAMESPACE services \
-  prometheus-endpoint-service \
-  -o jsonpath='{ .spec.ports[0].port }')
-
-cat <<EOF > nginx_prometheus1.conf
-worker_processes  5;
-
-events {
-  worker_connections  4096;
-}
-
-http {
-  index    index.html index.htm index.php;
-
-  default_type application/octet-stream;
-  sendfile     on;
-  tcp_nopush   on;
-  server_names_hash_bucket_size 128;
-
-  server {
-    listen       80;
-    server_name  domain1.com www.domain1.com;
-    root         html;
-
-    location / {
-      proxy_pass   http://${LB_IP}:${LB_PORT};
-    }
-  }
-}
-EOF
-
-# Run the nginx proxy on the same docker network where k8s is
-# also running on, by default it should be called kind
-docker run --name myprometheus1 -d -p 9080:80 --network kind \
-  -v $(pwd)/nginx_prometheus1.conf:/etc/nginx/nginx.conf:ro nginx:latest
+# Now start up the proxy container
+docker run --name istioproxy -d $PORTMAP --network kind \
+  -v /tmp/istioproxy.conf:/etc/nginx/nginx.conf:ro nginx:latest
