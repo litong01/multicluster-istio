e283c70f32cc7106154058fc2d059c28401f098d
Tong Li <litong01@us.ibm.com>
2022-03-16 15:17:54 -0400

    Added istio build script
    This script built istio using customized tag so that these
    images and binary can be used locally
    
    Signed-off-by: Tong Li <litong01@us.ibm.com>

---
+++ dev/buildistio.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# This script build istio using customized tag/version for development
+# purposes, once istio gets built, one can use the loadimage script
+# to load the images to kind cluster for tests
+
+function printHelp() {
+  echo "Usage: "
+  echo "    $0 -v 11.20-dev istioctl"
+  echo ""
+  echo "Where:"
+  echo "    -v|--version  - the version to be built as"
+  echo "    -h|--help     - print the usage of this script"
+  echo "    parameters should be istioctl docker.pilot docker.proxyv2 etc"
+}
+
+TARGETS=""
+
+# Handling parameters
+while [[ $# -gt 0 ]]; do
+  optkey="$1"
+  case $optkey in
+    -h|--help)
+      printHelp; exit 0;;
+    -v|--version)
+      VERSION="$2";shift;shift;;
+    *) # unknown option
+      TARGETS+="$1 ";shift;;
+  esac
+done
+
+if [[ -z "${TARGETS}" ]]; then
+  TARGETS="istioctl docker.pilot"
+fi
+
+if [[ ! -f "Makefile.core.mk" || ! -f "Makefile" ]]; then
+  echo "You may not in the Istio root directory"
+  exit 1
+else
+  if [[ -z "${VERSION}" ]]; then
+    # If nothing is setup, set it to the value in the Makefile.core.mk
+    VERSION=$(grep -F "export VERSION" Makefile.core.mk | awk '{ print $4}')
+  fi
+  export VERSION=$VERSION
+  export TAG=$VERSION
+  export HUB=istio
+  make $TARGETS
+fi
